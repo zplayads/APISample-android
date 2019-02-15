46a73cf16c13febcb8bdcbbf36dc82d1d074afcf
zmer007 <developer.lgd@gmail.com>
2019-02-15 19:54:36 +0800

    add new feature

--- app/src/main/java/com/zplay/playable/panosdk/MainActivity.java
+++ app/src/main/java/com/zplay/playable/panosdk/MainActivity.java
@@ -1,10 +1,12 @@
 package com.zplay.playable.panosdk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -31,14 +33,40 @@ public class MainActivity extends AppCompatActivity {
         if (trimContent.startsWith("http")) {
             WebActivity.launch(this, trimContent);
         } else {
-            trimContent = trimContent.replaceAll("\\\\\"", "\"");
-            trimContent = trimContent.replaceAll("\\\n", " ");
-            trimContent = trimContent.replaceAll("\\\t", " ");
-            trimContent = trimContent.replaceAll("\\\\r\\\\n", " ");
+            trimContent = getReformatData(trimContent);
             Log.d(TAG, "showWeb: " + trimContent);
             Log.d(TAG, "showWeb: " + "hello: \"");
             WebActivity.launch(this, trimContent, null, null);
         }
 
+    }
+
+    private String getReformatData(String raw) {
+        raw = raw.replaceAll("\\\\\"", "\"");
+        raw = raw.replaceAll("\\\n", " ");
+        raw = raw.replaceAll("\\\t", " ");
+        raw = raw.replaceAll("\\\\r\\\\n", " ");
+        return raw;
+    }
+
+    public void loadDoc2WebView(View view) {
+        String rawData = mEditText.getText().toString().trim();
+        if (TextUtils.isEmpty(rawData)) {
+            Toast.makeText(this, "input data is empty.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        WebViewHolder webViewHolder = WebViewHolder.getInstance(this);
+        webViewHolder.loadHtmlDoc(this, getReformatData(rawData), new WebViewHolder.WebViewListener() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.d(TAG, "onPageFinished: " + url);
+            }
+        });
+    }
+
+    public void showWeb2(View view) {
+        Intent i = new Intent(this, WebAttachActivity.class);
+        startActivity(i);
     }
 }
